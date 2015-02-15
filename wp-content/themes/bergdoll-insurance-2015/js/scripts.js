7fdae4a07c00ea77610835b1823c71bc2b4b1ed4
Joseph Bergdoll <joseph@osephj.com>
2015-02-15 18:38:19 -0500

    form switching

--- wp-content/themes/bergdoll-insurance-2015/js/scripts.js
+++ wp-content/themes/bergdoll-insurance-2015/js/scripts.js
@@ -11,20 +11,32 @@ $(document).ready(function() {
     $quoteButton = $('#start_quote');
 
   $stateSelect.change(function() {
-    var transitionDuration = 400;
+    var transitionDuration = 200;
     var showOpts = $(this).children('option:selected').attr('data-opts');
     var optsClass = '.' + showOpts;
-    $(optsClass).siblings('.active').removeClass('active');
-    setTimeout(function() {
-      $(optsClass).addClass('active');
-    }, transitionDuration);
+    $(optsClass).siblings('.active').fadeOut(transitionDuration, function() {
+      $(this).removeClass('active');
+      $(optsClass).fadeIn(transitionDuration/2).addClass('active');
+    });
+    $quoteButton.addClass('inactive').removeAttr('href');
   });
 
 
   $('.product_select').change(function() {
     if ($(this).parent('.state-opts').hasClass('active')) {
+      $widthSetter = $(this).next('.js_width_tmp');
+      $widthSetter.html($(this).children('option:selected').text());
+
+      var desiredWidth = $widthSetter.width();
+      $(this).width(desiredWidth);
       var dataHref = $(this).children('option:selected').attr('data-href');
       $quoteButton.attr('href', dataHref).removeClass('inactive');
+    }
+  });
+
+  $('.button').click(function(e){
+    if ($(this).hasClass('inactive')) {
+      e.preventDefault();
     }
   });
 
